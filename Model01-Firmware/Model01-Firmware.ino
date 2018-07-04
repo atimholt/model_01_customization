b255ceb8157dfb649ba580dcf85f2ef5a2ba3696
Tim Holt <atimholt@gmail.com>
2018-07-04 00:07:28 -0700

    Make the QWERTY mode match the physically rearranged thumb keys

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -73,14 +73,24 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+#ifdef CONFORM_QWERTY_THUMBS_TO_FACTORY_DEFAULT
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+#else
+   // Where I've moved the physical buttons to. The idea is, since I don’t have
+   // custom keycaps, it makes sense to have a mode that actually matches the labels.
+   Key_LeftGui, Key_Backspace, Key_LeftShift, Key_LeftControl,
+#endif
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(SWITCHER),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+#ifdef CONFORM_QWERTY_THUMBS_TO_FACTORY_DEFAULT
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+#else
+   Key_RightControl, Key_RightShift, Key_Spacebar, Key_LeftAlt,
+#endif
    ShiftToLayer(FUNCTION)),
 
 

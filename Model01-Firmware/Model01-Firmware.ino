cbc7f783dc7a5c447b120a69bc6c066b22551c8c
Tim Holt <atimholt@gmail.com>
2020-11-10 00:52:52 -0800

    Bring all macro code under one heading

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -76,15 +76,10 @@
 
 #include <Kaleidoscope-TopsyTurvy.h> // Shifted keys, unshifted by shift.
 
-// Key Layout
-//------------
+// Macros
+//--------
 
-/// Just a list of all the macros used by the Model 01's firmware.
-///
-/// The names aren't particularly important. What is important is that each
-/// is unique.
-///
-/// These are the names of your macros. They'll be used in two places:
+/// These are used in two places:
 ///
 /// 1. In your keymap definitions. There, you'll use the syntax `M(MACRO_NAME)`
 ///    to mark a specific keymap position as triggering `MACRO_NAME`
@@ -101,6 +96,72 @@ enum MyMacros
   MACRO_ANY
 };
 
+
+//   Per-Macro Definitions
+//  -----------------------
+
+/// Handles the 'firmware version info' macro.
+///
+/// When a key bound to the macro is pressed, this macro prints out the firmware
+/// build information as virtual keystrokes.
+///
+static void versionInfoMacro(uint8_t keyState)
+{
+  if (keyToggledOn(keyState)) {
+    Macros.type(PSTR("Keyboardio Model 01 - Kaleidoscope "));
+    Macros.type(PSTR(BUILD_INFORMATION));
+  }
+}
+
+/// Provides the functionality of the 'Any' key.
+///
+/// When the 'any key' macro is toggled on, a random alphanumeric key is
+/// selected. While the key is held, the function generates a synthetic
+/// keypress event repeating that randomly selected key.
+///
+static void anyKeyMacro(uint8_t keyState)
+{
+  static Key lastKey;
+  bool toggledOn = false;
+  if (keyToggledOn(keyState)) {
+    lastKey.setKeyCode(Key_A.getKeyCode() + (uint8_t)(millis() % 36));
+    toggledOn = true;
+  }
+
+  if (keyIsPressed(keyState)) {
+    Kaleidoscope.hid().keyboard().pressKey(lastKey, toggledOn);
+  }
+}
+
+//   Meta-Macro Definitions
+//  ------------------------
+
+/// Dispatches keymap events that are tied to a macro to that macro.
+///
+/// \param macroIndex
+///   The macro being called (the entry in the 'enum' earlier in this file).
+///
+/// \param keyState
+///   The state of the keyswitch. You can use the keyswitch state to figure out
+///   if the key has just been toggled on, is currently pressed or if it's just
+///   been released.
+///
+/// The 'switch' statement should have a 'case' for each entry of the macro
+/// enum. Each 'case' statement should call out to a function to handle the
+/// macro in question.
+///
+const macro_t*
+macroAction(uint8_t macroIndex, uint8_t keyState)
+{
+  switch (macroIndex) {
+    case MACRO_VERSION_INFO:  versionInfoMacro(keyState);                 break;
+    case MACRO_ANY:           anyKeyMacro(keyState);                      break;
+  }
+  return MACRO_NONE;
+}
+
+// Key Layout
+//------------
 
 /// The Model 01's key layouts are defined as 'keymaps'. By default, there are three
 /// keymaps: The standard QWERTY keymap, the "Function layer" keymap and the "Numpad"
@@ -149,15 +210,14 @@ enum Layers
   FUNCTION
 };
 
-
-// This comment toggles astyle off
-// *INDENT-OFF*
-
-// clang-format off
-
 KEYMAPS(
   // note: When editing the keymap, change tabstop to 30 (or use smart tabstops).
   // easy-grab vim commands (yi`:<c-r>"<cr>): `set ts=30` `set ts=2`
+  //
+  // clang-format off
+  //
+  // This comment toggles astyle off
+  // *INDENT-OFF*
 
   [DVORAK] = KEYMAP_STACKED(
     ___,	TOPSY(1),	TOPSY(2),	TOPSY(3),	TOPSY(4),	TOPSY(5),	Key_LEDEffectNext,
@@ -233,80 +293,11 @@ KEYMAPS(
     XXX
   )
 
+  // Re-enable astyle's indent enforcement.
+  // *INDENT-ON*
+  //
+  // clang-format on
 ) // KEYMAPS
-
-// clang-format on
-
-// Re-enable astyle's indent enforcement.
-// *INDENT-ON*
-
-//   Macros
-//  --------
-
-//     Per-Macro Definitions
-//    -----------------------
-
-/// Handles the 'firmware version info' macro.
-///
-/// When a key bound to the macro is pressed, this macro prints out the firmware
-/// build information as virtual keystrokes.
-///
-static void
-versionInfoMacro(uint8_t keyState)
-{
-  if (keyToggledOn(keyState)) {
-    Macros.type(PSTR("Keyboardio Model 01 - Kaleidoscope "));
-    Macros.type(PSTR(BUILD_INFORMATION));
-  }
-}
-
-/// Provides the functionality of the 'Any' key.
-///
-/// When the 'any key' macro is toggled on, a random alphanumeric key is
-/// selected. While the key is held, the function generates a synthetic
-/// keypress event repeating that randomly selected key.
-///
-static void
-anyKeyMacro(uint8_t keyState)
-{
-  static Key lastKey;
-  bool toggledOn = false;
-  if (keyToggledOn(keyState)) {
-    lastKey.setKeyCode(Key_A.getKeyCode() + (uint8_t)(millis() % 36));
-    toggledOn = true;
-  }
-
-  if (keyIsPressed(keyState)) {
-    Kaleidoscope.hid().keyboard().pressKey(lastKey, toggledOn);
-  }
-}
-
-//     Meta-Macro Definitions
-//    ------------------------
-
-/// Dispatches keymap events that are tied to a macro to that macro.
-///
-/// \param macroIndex
-///   The macro being called (the entry in the 'enum' earlier in this file).
-///
-/// \param keyState
-///   The state of the keyswitch. You can use the keyswitch state to figure out
-///   if the key has just been toggled on, is currently pressed or if it's just
-///   been released.
-///
-/// The 'switch' statement should have a 'case' for each entry of the macro
-/// enum. Each 'case' statement should call out to a function to handle the
-/// macro in question.
-///
-const macro_t*
-macroAction(uint8_t macroIndex, uint8_t keyState)
-{
-  switch (macroIndex) {
-    case MACRO_VERSION_INFO:  versionInfoMacro(keyState);                 break;
-    case MACRO_ANY:           anyKeyMacro(keyState);                      break;
-  }
-  return MACRO_NONE;
-}
 
 // Plugin Settings
 //-----------------
